ef9e68c2cf6d9241b3d626130a73e9d05401bef5
manuel1206 <31812975+manuel1206@users.noreply.github.com>
2017-09-22 21:29:25 -0500

    estoy subiendo el ejercicio 1

---
+++ ejercicio 1/src/pa/edu/uip/Main.java
@@ -0,0 +1,32 @@
+package pa.edu.uip;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos  a java! ");
+
+   // ENTEROS
+        byte a;
+        short b;
+        int c = 27;
+        long d;
+
+   // DECIMALES
+        float e;
+        double f = 0.0;
+
+     // TEXTO
+     char g = 'x' ;
+     String i = "x";
+
+
+     // BOLEANO
+        boolean  vivo = true;
+        vivo =false;
+
+
+
+    }
+
+
+}
